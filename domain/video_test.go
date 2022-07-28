cb8007475dc51e1d25963f624f10db808158e6e6
Leandro Koller <leandro@brasilfone.com.br>
2022-07-28 16:43:07 -0300

    Initial commit.

---
+++ domain/video_test.go
@@ -0,0 +1,43 @@
+package domain_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/leandro-koller-bft/video-encoder/domain"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidateIfVideoIsEmpty(t *testing.T) {
+	video := domain.NewVideo()
+	err := video.Validate()
+
+	require.Error(t, err)
+}
+
+func TestVideoIsNotUuid(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = "123"
+	video.ResourceID = "a"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+
+	require.Error(t, err)
+}
+
+func TestVideoValidation(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = uuid.NewV4().String()
+	video.ResourceID = "a"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+
+	require.Nil(t, err)
+}
