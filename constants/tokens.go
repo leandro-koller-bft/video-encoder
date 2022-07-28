50ed02d5aa6a1810b30627ca48a18b57861391b7
Leandro Koller <leandro@brasilfone.com.br>
2022-07-28 18:01:12 -0300

    Adiciona as configuracoes de banco de dados

---
+++ constants/tokens.go
@@ -0,0 +1,5 @@
+package constants
+
+const (
+	TEST_TOKEN = "test"
+)
---
+++ framework/database/db.go
@@ -0,0 +1,69 @@
+package database
+
+import (
+	"log"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/leandro-koller-bft/video-encoder/constants"
+	"github.com/leandro-koller-bft/video-encoder/domain"
+	_ "github.com/lib/pq"
+)
+
+type Database struct {
+	DB            *gorm.DB
+	DSN           string
+	DSNTest       string
+	DBType        string
+	DBTypeTest    string
+	Debug         bool
+	AutoMigrateDB bool
+	Env           string
+}
+
+func NewDB() *Database {
+	return &Database{}
+}
+
+func NewDBTest() *gorm.DB {
+	dbInstance := NewDB()
+
+	dbInstance.Env = constants.TEST_TOKEN
+	dbInstance.DBTypeTest = "sqlite3"
+	dbInstance.DSNTest = ":memory:"
+	dbInstance.AutoMigrateDB = true
+	dbInstance.Debug = true
+
+	connection, err := dbInstance.Connect()
+
+	if err != nil {
+		log.Fatalf("Test db error: %v", err)
+	}
+
+	return connection
+}
+
+func (db *Database) Connect() (*gorm.DB, error) {
+	var err error
+
+	if db.Env != constants.TEST_TOKEN {
+		db.DB, err = gorm.Open(db.DBType, db.DSN)
+	} else {
+		db.DB, err = gorm.Open(db.DBTypeTest, db.DSNTest)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if db.Debug {
+		db.DB.LogMode(true)
+	}
+
+	if db.AutoMigrateDB {
+		db.DB.AutoMigrate(&domain.Video{}, &domain.Job{})
+		db.DB.AddForeignKey("video_id", "videos (id)", "CASCADE", "CASCADE")
+	}
+
+	return db.DB, nil
+}
